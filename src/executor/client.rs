0735d53068a25d6e1491721a9eab1ec72594d776
Вова Ерущенко <scorceny7@gmail.com>
2025-07-03 20:56:03 +0200

    add send_tr.._signer to avoid code dublication

--- src/executor/client.rs
+++ src/executor/client.rs
@@ -33,4 +33,21 @@ impl SolanaClient {
     pub fn payer_pubkey(&self) -> Pubkey {
         self.payer.pubkey()
     }
+
+    pub fn send_transaction_with_signers(
+        &self,
+        instructions: Vec<solana_sdk::instruction::Instruction>,
+        signers: &[&dyn Signer],
+    ) -> Result<String> {
+        let recent_blockhash = self.rpc_client.get_latest_blockhash()?;
+        let transaction = Transaction::new_signed_with_payer(
+            &instructions,
+            Some(&self.payer.pubkey()),
+            signers,
+            recent_blockhash,
+        );
+
+        let signature = self.rpc_client.send_and_confirm_transaction(&transaction)?;
+        Ok(signature.to_string())
+    }
 }
