53e1f956b58c861a2c638f9d1394df6193490be2
Вова Ерущенко <scorceny7@gmail.com>
2025-07-03 18:10:40 +0200

    add interface for CLI

---
+++ src/cli/interface.rs
@@ -0,0 +1,35 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "Spl-Airdropper")]
+#[command(about = "A CLI tool for creating SPL tokens and airdropping them to recipients")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Create a new SPL token mint")]
+    CreateToken {
+        #[arg(long, help = "Number of decimal places for the token")]
+        decimals: u8,
+    },
+    #[command(about = "Airdrop tokens to a list of recipients")]
+    Airdrop {
+        #[arg(long, help = "Token mint address")]
+        token_mint: String,
+        #[arg(long, help = "Amount of tokens to airdrop to each recipient")]
+        amount: u64,
+        #[arg(long, help = "Path to file containing recipient addresses")]
+        recipients: PathBuf,
+    },
+    #[command(about = "Check token balance for an address")]
+    CheckBalance {
+        #[arg(long, help = "Token mint address")]
+        token_mint: String,
+        #[arg(long, help = "Wallet address to check balance for")]
+        address: String,
+    },
+}
